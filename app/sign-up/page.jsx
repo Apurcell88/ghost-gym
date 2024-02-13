7df09fae98b5d2533f149601dcf7abccb4259e3f
Adam Purcell <apurcell88@gmail.com>
2024-02-13 12:23:06 -0600

    Added styling to sign up form

--- app/sign-up/page.jsx
+++ app/sign-up/page.jsx
@@ -1,66 +1,150 @@
 const signUpForm = () => {
+  const handleChange = (e) => {};
+
+  const handleSubmit = (e) => {};
+
+  const signUpData = {
+    firstName: "",
+    lastName: "",
+    username: "",
+    password: "",
+    confirmPassword: "",
+    personalTrainer: false,
+    age: 1,
+    phoneNumber: "",
+    email: "",
+  };
+
   return (
-    <section className="bg-gray-800 h-screen">
+    <section className="bg-gray-800 h-screen flex flex-col items-center">
       <h1 className="text-center text-white text-3xl py-10">
         Sign Up For Ghost Gym
       </h1>
-      <form>
+      <form className="flex flex-col gap-5 text-white">
         <div>
-          <label for="firstName">First Name: </label>
-          <input type="text" id="firstName" name="firstName" required />
-        </div>
-
-        <div>
-          <label for="lastName">Last Name: </label>
-          <input type="text" id="lastName" name="lastName" required />
-        </div>
-
-        <div>
-          <label for="userName">Username: </label>
-          <input type="text" id="username" name="username" required />
-        </div>
-
-        <div>
-          <label for="password">Password: </label>
-          <input type="password" id="password" name="password" required />
-        </div>
-
-        <div>
-          <label for="confirmPassword">Confirm Password: </label>
+          <label for="firstName" className="sign-up-form--label">
+            First Name:{" "}
+          </label>
           <input
-            type="password"
-            id="confirmPassword"
-            name="confirmPassword"
+            type="text"
+            id="firstName"
+            name="firstName"
+            className="sign-up-form--input"
             required
           />
         </div>
 
         <div>
-          <label for="age">Are you a personal trainer? </label>
-          <select name="trainer" id="trainer">
+          <label for="lastName" className="sign-up-form--label">
+            Last Name:{" "}
+          </label>
+          <input
+            type="text"
+            id="lastName"
+            name="lastName"
+            className="sign-up-form--input"
+            required
+          />
+        </div>
+
+        <div>
+          <label for="userName" className="sign-up-form--label">
+            Username:{" "}
+          </label>
+          <input
+            type="text"
+            id="username"
+            name="username"
+            className="sign-up-form--input"
+            required
+          />
+        </div>
+
+        <div>
+          <label for="password" className="sign-up-form--label">
+            Password:{" "}
+          </label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            className="sign-up-form--input"
+            required
+          />
+        </div>
+
+        <div>
+          <label for="confirmPassword" className="sign-up-form--label">
+            Confirm Password:{" "}
+          </label>
+          <input
+            type="password"
+            id="confirmPassword"
+            name="confirmPassword"
+            className="sign-up-form--input"
+            required
+          />
+        </div>
+
+        <div>
+          <label for="age" className="sign-up-form--label">
+            Are you a personal trainer?{" "}
+          </label>
+          <select name="trainer" id="trainer" className="text-black">
             <option value="no">No</option>
             <option value="yes">Yes</option>
           </select>
         </div>
 
         <div>
-          <label for="age">Age: </label>
-          <input type="number" id="age" name="age" required />
+          <label for="age" className="sign-up-form--label">
+            Age:{" "}
+          </label>
+          <input
+            type="number"
+            id="age"
+            name="age"
+            className="sign-up-form--input"
+            required
+          />
         </div>
 
         <div>
-          <label for="phoneNumber">Phone Number: </label>
-          <input type="text" id="phoneNumber" name="phoneNumber" required />
+          <label for="phoneNumber" className="sign-up-form--label">
+            Phone Number:{" "}
+          </label>
+          <input
+            type="text"
+            id="phoneNumber"
+            name="phoneNumber"
+            className="sign-up-form--input"
+            required
+          />
         </div>
 
         <div>
-          <label for="email">Email: </label>
-          <input type="email" id="email" name="email" required />
+          <label for="email" className="sign-up-form--label">
+            Email:{" "}
+          </label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            className="sign-up-form--input"
+            required
+          />
         </div>
 
         <div>
-          <label for="consent">Consent: </label>
+          <label for="consent" className="sign-up-form--label">
+            Consent:{" "}
+          </label>
           <input type="checkbox" id="consent" name="consent" required />
+          <span> By checking this box...</span>
+        </div>
+
+        <div className="self-center text-lg">
+          <input type="submit" className="hover:cursor-pointer" />
         </div>
       </form>
     </section>
