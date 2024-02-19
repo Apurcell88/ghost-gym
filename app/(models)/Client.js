53e6284d1dd4c5f8b117608923e196ee613b1361
Adam Purcell <apurcell88@gmail.com>
2024-02-19 12:35:00 -0600

    Added Client Schema

---
+++ app/(models)/Client.js
@@ -0,0 +1,19 @@
+import mongoose, { Schema } from "mongoose";
+
+mongoose.connect(process.env.MONGODB_URI);
+mongoose.Promise = global.Promise;
+
+const clientSchema = new Schema({
+  firstName: String,
+  lastName: String,
+  username: String,
+  password: String,
+  age: Number,
+  phoneNumber: String,
+  email: String,
+  fitnessGoals: String,
+});
+
+const Client = mongoose.models.Client || mongoose.model("Client", clientSchema);
+
+export default Client;
