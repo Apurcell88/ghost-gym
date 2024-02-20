b641906ddb48ee449db54bddb3916dabf4673246
Adam Purcell <apurcell88@gmail.com>
2024-02-20 16:09:24 -0600

    Added SignIn and styling

---
+++ app/sign-in/page.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+
+const SignIn = () => {
+  return (
+    <div className="bg-slate-800 h-[90vh] flex flex-col justify-center">
+      <h1 className="text-center text-3xl pb-8 text-white">Please Sign In</h1>
+      <form className="flex flex-col items-center gap-5">
+        <div>
+          <label htmlFor="username" className="sign-up-form--label">
+            Username:{" "}
+          </label>
+          <input
+            type="text"
+            name="username"
+            id="username"
+            className="sign-up-form--input"
+          />
+        </div>
+
+        <div>
+          <label htmlFor="password" className="sign-up-form--label">
+            Password:{" "}
+          </label>
+          <input
+            type="password"
+            name="password"
+            id="password"
+            className="sign-up-form--input"
+          />
+        </div>
+
+        <div>
+          <input
+            type="submit"
+            className="text-lg text-white hover:cursor-pointer"
+          />
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default SignIn;
